03e3585f229cb3f7ec63f0173e134315c50f0d3e
Dew Parin <parinsura@gmail.com>
2021-09-19 21:16:22 +0700

    Remove unnecessary comment from `TxPhoneApp`.

--- lib/tx_phone_app.dart
+++ lib/tx_phone_app.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class TxPhoneApp extends StatelessWidget {
   const TxPhoneApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
