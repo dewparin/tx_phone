4485a75d53e7b033ea3cbf2a9e94206780514395
Dew Parin <parinsura@gmail.com>
2021-09-19 21:41:05 +0700

    Add `mock_data.dart` for unit test.

---
+++ test/phone_guide_feature/mock_data.dart
@@ -0,0 +1,32 @@
+import 'package:tx_phone/entity/phone.dart';
+
+final phoneA = Phone(
+  id: 1,
+  name: 'A',
+  price: 100.0,
+  rating: 1.4,
+  thumbImageUrl: '',
+  description: 'First Phone',
+  brand: 'A',
+  isFavorite: false,
+);
+final phoneB = Phone(
+  id: 2,
+  name: 'B',
+  price: 200.0,
+  rating: 3.4,
+  thumbImageUrl: '',
+  description: 'Second Phone',
+  brand: 'B',
+  isFavorite: false,
+);
+final phoneC = Phone(
+  id: 3,
+  name: 'C',
+  price: 300.0,
+  rating: 2.4,
+  thumbImageUrl: '',
+  description: 'Third Phone',
+  brand: 'C',
+  isFavorite: false,
+);
