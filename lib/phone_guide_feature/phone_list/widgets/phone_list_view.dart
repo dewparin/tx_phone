7d14ea3ce1e8bdaebe973c8418fb126b12e67483
Dew Parin <parinsura@gmail.com>
2021-09-19 22:44:22 +0700

    Modify `PhoneListView` to support 2 layout versions: Regular and Favorite.

--- lib/phone_guide_feature/phone_list/widgets/phone_list_view.dart
+++ lib/phone_guide_feature/phone_list/widgets/phone_list_view.dart
@@ -5,33 +5,73 @@ import 'package:tx_phone/entity/phone.dart';
 import 'package:tx_phone/phone_guide_feature/phone_list/phone_list_screen.dart';
 import 'package:tx_phone/phone_guide_feature/phone_list/widgets/phone_list_item.dart';
 
-class PhoneListView extends ConsumerWidget {
+enum PhoneListViewLayout {
+  regular,
+  favorite,
+}
+
+class PhoneListView extends StatelessWidget {
+  final PhoneListViewLayout layout;
+
   const PhoneListView({
     Key? key,
+    this.layout = PhoneListViewLayout.regular,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) => Builder(
-        builder: (BuildContext context) {
-          final phones = watch(sortedPhoneListProvider);
-          if (phones.isNotEmpty) {
-            return Container(
-              padding: const EdgeInsets.symmetric(horizontal: smallPadding),
-              child: ListView.separated(
-                itemCount: phones.length,
-                itemBuilder: (context, index) {
-                  return _buildPhoneListItem(phones[index]);
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(height: smallPadding);
-                },
-              ),
-            );
-          } else {
-            return const Center(child: Text('No Phones Here :-('));
-          }
-        },
-      );
+  Widget build(BuildContext context) {
+    if (layout == PhoneListViewLayout.favorite) {
+      return _FavoritePhoneListView();
+    } else {
+      return _RegularPhoneListView();
+    }
+  }
+}
+
+abstract class _BasePhoneListView extends ConsumerWidget {
+  const _BasePhoneListView({
+    Key? key,
+    this.layout = PhoneListViewLayout.regular,
+  }) : super(key: key);
+
+  final PhoneListViewLayout layout;
+
+  RootProvider getSourceProvider();
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Builder(
+      builder: (BuildContext context) {
+        final phones = watch(getSourceProvider());
+        if (phones.isNotEmpty) {
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: smallPadding),
+            child: ListView.separated(
+              itemCount: phones.length,
+              itemBuilder: (context, index) {
+                return _buildPhoneListItem(phones[index]);
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: smallPadding);
+              },
+            ),
+          );
+        } else {
+          return const Center(child: Text('No Phones Here :-('));
+        }
+      },
+    );
+  }
 
   Widget _buildPhoneListItem(Phone phone) => PhoneListItem(phone: phone);
+}
+
+class _RegularPhoneListView extends _BasePhoneListView {
+  @override
+  RootProvider getSourceProvider() => sortedPhoneListProvider;
+}
+
+class _FavoritePhoneListView extends _BasePhoneListView {
+  @override
+  RootProvider getSourceProvider() => sortedFavoritePhoneList;
 }
