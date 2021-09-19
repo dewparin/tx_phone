6f252a724209c42fe9fe28675acf97d6117f970a
Dew Parin <parinsura@gmail.com>
2021-09-19 21:38:46 +0700

    Implement `getPhonesFutureProvider` to fetch phone list from repository and store in the phone list state notifier.

--- lib/phone_guide_feature/phone_list/phone_list_model.dart
+++ lib/phone_guide_feature/phone_list/phone_list_model.dart
@@ -1,5 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tx_phone/data/phone_repository.dart';
 import 'package:tx_phone/entity/phone.dart';
+
+final getPhonesFutureProvider = FutureProvider.autoDispose((ref) async {
+  final cancelToken = CancelToken();
+  ref.onDispose(cancelToken.cancel);
+  final repository = ref.watch(repositoryProvider);
+  final phones = await repository.fetchPhones(cancelToken);
+  ref.maintainState = true;
+  ref.read(phoneListProvider.notifier).setNewPhoneList(phones);
+  return phones;
+});
 
 final phoneListProvider = StateNotifierProvider<PhoneList, List<Phone>>((ref) {
   return PhoneList([]);
