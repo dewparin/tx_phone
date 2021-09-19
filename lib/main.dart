276ffeb24468347719c1f347d3ca0a74f4cb793d
Dew Parin <parinsura@gmail.com>
2021-09-19 21:19:25 +0700

    Set `ProviderScope` as a root widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tx_phone/tx_phone_app.dart';
 
 void main() {
-  runApp(const TxPhoneApp());
+  runApp(const ProviderScope(
+    child: TxPhoneApp(),
+  ));
 }
