52aa64c0acb7d2e3ad31e99fe84553974030191b
Dew Parin <parinsura@gmail.com>
2021-09-20 12:26:15 +0700

    Modify `PhoneRepository` to support fetch phone images.

--- lib/data/phone_repository.dart
+++ lib/data/phone_repository.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tx_phone/data/rest_client.dart';
 import 'package:tx_phone/entity/phone.dart';
+import 'package:tx_phone/entity/phone_image.dart';
 
 final repositoryProvider =
     Provider((ref) => PhoneRepository(ref.read(restClientProvider)));
@@ -13,4 +14,8 @@ class PhoneRepository {
 
   Future<List<Phone>> fetchPhones(CancelToken cancelToken) =>
       _restClient.getPhones(cancelToken);
+
+  Future<List<PhoneImage>> fetchPhoneImages(
+          int phoneId, CancelToken cancelToken) =>
+      _restClient.getPhoneImages(phoneId, cancelToken);
 }
