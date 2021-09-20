49b835680f489073bdc1987155530a9a30f1a3a2
Dew Parin <parinsura@gmail.com>
2021-09-20 12:50:25 +0700

    Add `getPhoneImagesFutureProvider` to fetch phone images from repository.

--- lib/phone_guide_feature/phone_item_details/phone_item_details_model.dart
+++ lib/phone_guide_feature/phone_item_details/phone_item_details_model.dart
@@ -1,8 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tx_phone/data/phone_repository.dart';
 import 'package:tx_phone/entity/phone.dart';
+import 'package:tx_phone/entity/phone_image.dart';
 import 'package:tx_phone/phone_guide_feature/phone_list/phone_list_model.dart';
 
 final phoneDetailsProvider = Provider.family<Phone, int>((ref, phoneId) {
   final allPhones = ref.watch(phoneListProvider);
   return allPhones.firstWhere((phone) => phone.id == phoneId);
+});
+
+final getPhoneImagesFutureProvider = FutureProvider.autoDispose
+    .family<List<PhoneImage>, int>((ref, phoneId) async {
+  final cancelToken = CancelToken();
+  ref.onDispose(cancelToken.cancel);
+  final repository = ref.watch(repositoryProvider);
+  final images = await repository.fetchPhoneImages(phoneId, cancelToken);
+  ref.maintainState = true;
+  return images;
 });
