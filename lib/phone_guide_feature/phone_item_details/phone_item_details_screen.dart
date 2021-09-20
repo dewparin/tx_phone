9c7207ed17a09baf9182a652fd0318405572b304
Dew Parin <parinsura@gmail.com>
2021-09-20 12:49:26 +0700

    Modify `PhoneItemDetailsScreen` to display phone information without fetching images.

--- lib/phone_guide_feature/phone_item_details/phone_item_details_screen.dart
+++ lib/phone_guide_feature/phone_item_details/phone_item_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tx_phone/constant.dart';
+import 'package:tx_phone/entity/phone.dart';
+import 'package:tx_phone/phone_guide_feature/phone_item_details/phone_item_details_model.dart';
 
 class PhoneItemDetailsScreenArguments {
   final int phoneId;
@@ -16,8 +19,73 @@ class PhoneItemDetailsScreen extends ConsumerWidget {
   Widget build(BuildContext context, ScopedReader watch) {
     final args = ModalRoute.of(context)!.settings.arguments
         as PhoneItemDetailsScreenArguments;
-    return Center(
-      child: Text('${args.phoneId}'),
+    final phone = watch(phoneDetailsProvider(args.phoneId));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(phone.name),
+      ),
+      body: Column(children: [
+        Expanded(
+          child: _buildCoverSection(context, phone, watch),
+        ),
+        Expanded(
+          flex: 2,
+          child: _buildContentSection(phone, context),
+        ),
+      ]),
     );
   }
+
+  Stack _buildCoverSection(
+          BuildContext context, Phone phone, ScopedReader watch) =>
+      Stack(
+        children: [
+          const Center(child: CircularProgressIndicator()),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.all(defaultPadding),
+              color: Colors.black45,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildOverlayText(context, 'Rating: ${phone.rating}'),
+                  _buildOverlayText(context, 'Price: \$${phone.price}'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Text _buildOverlayText(BuildContext context, String text) => Text(
+        text,
+        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+              color: Colors.white,
+            ),
+      );
+
+  Container _buildContentSection(Phone phone, BuildContext context) =>
+      Container(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              phone.name,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: largePadding),
+            ),
+            Text(
+              phone.description,
+              textAlign: TextAlign.justify,
+              textScaleFactor: 1.1,
+            ),
+          ],
+        ),
+      );
 }
